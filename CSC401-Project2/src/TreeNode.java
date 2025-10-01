feaf0b8f1593e1672d0189dface4201ca3dc9cae
Kebur <kb.gedion@gmail.com>
2025-10-01 00:01:42 +0000

    CSC401-P1: Build TreeNode class

---
+++ CSC401-Project2/src/TreeNode.java
@@ -0,0 +1,23 @@
+public class TreeNode {
+    private int value;
+    private TreeNode left; 
+    private TreeNode right;
+
+    public TreeNode(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    public int getValue() { return this.value; }
+    public TreeNode getLeft() { return this.left; }
+    public TreeNode getRight() { return this.right; }
+    public void setValue(int value) { this.value = value; }
+    public void setLeft(TreeNode left) { this.left = left; }
+    public void setRight(TreeNode right) { this.right = right; }
+
+    @Override
+    public String toString() {
+        return "Node(" + value + ")"; 
+    }
+
