9b8d681ea3b4a35357129b3545b4d4a60081ee94
Kebur <kb.gedion@gmail.com>
2025-12-01 20:53:58 +0000

    401-P4: write the HuffManNode class

---
+++ csc401/CSC401-Project4/HuffmanNode.java
@@ -0,0 +1,38 @@
+
+public class HuffmanNode implements Comparable<HuffmanNode> {
+    char character;
+    int frequency;
+    HuffmanNode left;
+    HuffmanNode right;
+
+    // Consturctor for leaf node
+    public HuffmanNode(char character, int frequency) {
+        this.character = character;
+        this.frequency = frequency;
+        this.left = null;
+        this.right = null;
+    }
+
+    // Consturctor for internal node
+    public HuffmanNode(int frequency) {
+        this.character = '\O';
+        this.frequency = frequency;
+        this.left = null;
+        this.right = null;
+    }
+
+    public boolean isLeaf() { return left == null && right == null; }
+
+    @Override
+    public int compareTo(HuffmanNode other) { return this.frequency - other.frequency; }
+
+    @Override
+    public String toString() {
+        if (isLeaf()) {
+            return "Leaf('" + character + "', freq=" + frequency + ")";
+        }
+
+        return "Internal (freq=" + frequency + ")";
+    }
+}
+
